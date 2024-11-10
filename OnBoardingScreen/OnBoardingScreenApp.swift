ab5316c7116c9657b2b27080b018d1a623acd892
Amanda Caroline da Silva Rodrigues <amandacsr10@gmail.com>
2024-11-10 08:17:27 -0300

    Initial Commit

---
+++ OnBoardingScreen/OnBoardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingScreenApp.swift
+//  OnBoardingScreen
+//
+//  Created by Amanda Rodrigues on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnBoardingScreenTests/OnBoardingScreenTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingScreenTests.swift
+//  OnBoardingScreenTests
+//
+//  Created by Amanda Rodrigues on 10/11/24.
+//
+
+import Testing
+@testable import OnBoardingScreen
+
+struct OnBoardingScreenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
